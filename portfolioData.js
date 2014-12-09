31b2dd753070be90b98029a97be90c1195254413
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2014-12-09 18:37:01 +0000

    Moved data into JSON format

---
+++ portfolioData.js
@@ -0,0 +1,58 @@
+var portfolioData = [
+{
+	title: "Befittd",
+	subtitle:  "Internship - Summer 2014",
+	img: 'befittd_portfolio_cover.png',
+	description : "An overview of what I've done in my internship at Befittd, a fashion-tech startup based in London.",
+	link: 'befittd.html'
+},
+{
+	title: "Startup Circus",
+	subtitle:  "A work in progress.",
+	img: 'startup_circus_cover.png',
+	description : "<p>Startup Circus is a new concept of careers fair, which is internship focused and targets students in their first year of University.</p><p>The truth is that most STEM graduates (specially from Imperial) will either be swallowed by the world of corporate banking and finance, or end up doing some kind of software related job.</p><p>The idea is that if people get involved in the world of work early on, by the time they graduate there will be no need for careers fairs, because they'll already know where to work.</p>",
+	link: 'http://startupcircus.co.uk'
+}, 
+{
+	title: "Internship Presentation",
+	subtitle:  "The most important things I have learnt in my internship.",
+	img: 'presentation_cover.png',
+	description : "<p>This is a presentation I gave at the end of the <a href=\"http://www.joinef.com/founder-school-internship/\" target=\"_blank\">EF Summer Internship Programme</a> 2014.</p><p>This was also a cool opportunity to try a cool javaScript library <a href=\"http://lab.hakim.se/reveal-js/#/\" target=\"_blank\">reveal.js</a>.</p><p><i>This is a presentation where the slides support the speech, and therefore it's quite hard to follow without guidance.</i> <b>But it's very pretty so go on and click.</b></p>",
+	link: 'ef_presentation/index.html'
+}, 
+{
+	title: "Social Network Analysis",
+	subtitle:  "Year One Group Project",
+	img: 'social_network_portfolio.png',
+	description : "<p>I made this as part of the \"Topics in Computing\" project at Imperial College London. It explains the science between social network phenomena such as influence, clustering and centrality.</p><p>My group and I won the Programme Prize for Best Group Project on Social Network Analysis.</p>",
+	link: 'topics/'
+}, 
+{
+	title: "Resident Designer at Wilkinson and Weeks Hall",
+	subtitle:  "(literally resident.) (I live here.)",
+	img: 'wwsquared.png',
+	description : "<p>I've been producing print and digital designs for my Hall of Residence for almost a year now.</p><p>It all started with the design of the new unified logo which was adopted as the official logo last year.</p><p>I have particularly enjoyed this because I got to design and shape the identity of this place in which 200+ new students come to live in every year.</p>",
+	link: ''
+}, 
+{
+	title: "Photography",
+	subtitle:  "",
+	img: 'photography_cover.png',
+	description : "<p>I've been pressing shutter buttons for a fair amount of years and I've got some of my pictures up on flickr.</p><p>I've been interested in taking pictures of works of art, since they are often misrepresented in photography. I find the way we look at screens to be completely opposite to the way we look at art in museums and I've been exploring how the two can meet halfway.</p>",
+	link: 'http://flickr.com/lorenzop'
+}, 
+{
+	title: "Presentation Design",
+	subtitle:  "A short presentation I gave at the EF Unconference.",
+	img: 'pres_design_cover.png',
+	description : "<p>People always seem to appreciate my presentations. I have a bunch of <b>FREE</b> tricks to teach that will make any presentation instantly so much better. And I'd like to share them with the world <small>so that I don't have to sit through 40 slides of Comic Sans <i>ever again</i>.</small></p>",
+	link: 'presentation-design.html'
+},
+{
+	title: "Hall Senior",
+	subtitle:  "Wilkinson & Weeks 2014-2015",
+	img: 'senior_cover.png',
+	description : "Being on the senior team has been more challenging than I thought it would be. Planning social life for 200+ students and managing your own student life is a task and a half.",
+	link: ''
+}
+];
